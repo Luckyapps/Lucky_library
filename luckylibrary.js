b5df2236c369bcc6e031f7b999e75b3aeb5cc40f
luckystriker16 <75381524+luckystriker16@users.noreply.github.com>
2022-07-18 23:24:35 +0200

    Update luckylibrary.js
    added various functions

--- luckylibrary.js
+++ luckylibrary.js
@@ -7,7 +7,7 @@ function element_add(wert, element, mode){ //Etwas (z.B. Text) zu einem html ele
         mode = undefined;
       }else{
         document.getElementById(element).innerHTML = wert;
-        console.log("contaiener add to element");
+        console.log("container add to element");
         element = undefined;
       }
     }else{
@@ -32,4 +32,45 @@ function get_add(adress){ //Hilfsfunktion gehört zu data_get()
 async function data_get(adress){ //Api Abfrage zurückgeben
      var data_temp = await get_add(adress);
      return data_temp;
+}
+
+function sleep(ms) { //Sleep funktion, wird ausgelöst mit: await sleep(ms) !!Aufrufende funktion muss asynchron sein!!
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function createHTML(htmlString) { //HTML element erstellen
+  var div = document.createElement('div');
+  div.innerHTML = htmlString.trim();
+
+  // Change this to div.childNodes to support multiple top-level nodes.
+  return div.firstChild;
+}
+
+async function scriptLoader(path, callback){ //Ein JS script einbetten
+  var script = document.createElement('script');
+  script.type = "text/javascript";
+  //script.async = true;
+  script.src = path;
+  script.onload = function(){  
+    if(typeof(callback) == "function"){
+      callback();
+    }
+  }
+  try
+  {
+    var scriptOne = document.getElementsByTagName('script')[0];
+    scriptOne.parentNode.insertBefore(script, scriptOne);
+  }
+  catch(e)
+  {
+    document.getElementsByTagName("head")[0].appendChild(script);
+  }
+}
+
+function cssLoader(file){ //Ein CSS stylesheet einbetten
+    var link = document.createElement("link");
+    link.href = file;
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    document.getElementsByTagName("head")[0].appendChild(link);
 }
